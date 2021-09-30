f8dcf04156606255440f1e9dad2f53dd5cf26578
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-09-30 12:02:28 +0900

    feat: implement get tweets

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tweet-image-downloader
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 var userNameF = flag.String("u", "kz_dev_", "input user name")
 var keywordF = flag.String("k", "最新話です!!", "input keyword which target tweet has")
@@ -25,6 +31,17 @@ func main() {
 		UserName: *userNameF,
 		Keyword:  *keywordF,
 		Max:      *max,
+	}
+
+	// 2. 条件より、目的の画像を持つツイートを取得
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error: loading .env file")
+	}
+	client := NewClient(os.Getenv("BEARER_TOKEN"))
+	res, err := client.GetTweets(con)
+	if err != nil {
+		log.Fatal(err)
 	}
 	// TODO
 }
