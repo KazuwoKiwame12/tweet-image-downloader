a7992a41bf5f29eb1a9f6688ac5880b933d8b55e
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-09-30 21:32:51 +0900

    test: change field name

--- utility/param_test.go
+++ utility/param_test.go
@@ -9,45 +9,45 @@ import (
 
 func Test_ValidateEmptyParamValue(t *testing.T) {
 	tests := []struct {
-		name     string
-		instance *utility.ParamBuilder
-		want     error
+		name  string
+		input *utility.ParamBuilder
+		want  error
 	}{
 		{
-			name:     "failed cause of expansion Parameter Value is empty",
-			instance: utility.NewParamBuilder().Expansions(entity.ExpansionFields{}),
-			want:     entity.ErrorEmptyParameterValue,
+			name:  "failed cause of expansion Parameter Value is empty",
+			input: utility.NewParamBuilder().Expansions(entity.ExpansionFields{}),
+			want:  entity.ErrorEmptyParameterValue,
 		},
 		{
-			name:     "failed cause of media.fields Parameter Value is empty",
-			instance: utility.NewParamBuilder().MediaFields(entity.MediaFields{}),
-			want:     entity.ErrorEmptyParameterValue,
+			name:  "failed cause of media.fields Parameter Value is empty",
+			input: utility.NewParamBuilder().MediaFields(entity.MediaFields{}),
+			want:  entity.ErrorEmptyParameterValue,
 		},
 		{
-			name:     "failed cause of query Parameter Value is empty",
-			instance: utility.NewParamBuilder().Query(entity.QueryFields{}),
-			want:     entity.ErrorEmptyParameterValue,
+			name:  "failed cause of query Parameter Value is empty",
+			input: utility.NewParamBuilder().Query(entity.QueryFields{}),
+			want:  entity.ErrorEmptyParameterValue,
 		},
 		{
-			name:     "failed cause of tweet.fields Parameter Value is empty",
-			instance: utility.NewParamBuilder().TweetFields(entity.TweetFields{}),
-			want:     entity.ErrorEmptyParameterValue,
+			name:  "failed cause of tweet.fields Parameter Value is empty",
+			input: utility.NewParamBuilder().TweetFields(entity.TweetFields{}),
+			want:  entity.ErrorEmptyParameterValue,
 		},
 		{
 			name: "failed cause of tweet.fields Parameter Value is empty, but query Parameter Value is not empty",
-			instance: utility.NewParamBuilder().TweetFields(entity.TweetFields{}).Query(entity.QueryFields{
+			input: utility.NewParamBuilder().TweetFields(entity.TweetFields{}).Query(entity.QueryFields{
 				entity.QueryFieldHasImages,
 			}),
 			want: entity.ErrorEmptyParameterValue,
 		},
 		{
-			name:     "success only one parameter",
-			instance: utility.NewParamBuilder().Query(entity.QueryFields{entity.QueryFieldHasImages}),
-			want:     nil,
+			name:  "success only one parameter",
+			input: utility.NewParamBuilder().Query(entity.QueryFields{entity.QueryFieldHasImages}),
+			want:  nil,
 		},
 		{
 			name: "success when multiple parameters",
-			instance: utility.NewParamBuilder().
+			input: utility.NewParamBuilder().
 				Query(entity.QueryFields{
 					entity.QueryFieldHasImages,
 					entity.QueryFieldFrom("test"),
@@ -62,7 +62,7 @@ func Test_ValidateEmptyParamValue(t *testing.T) {
 
 	for i, test := range tests {
 		t.Run(fmt.Sprintf("%d: %s", i, test.name), func(t *testing.T) {
-			if str, err := utility.ExportValidateEmptyParamValue(test.instance); err != test.want {
+			if str, err := utility.ExportValidateEmptyParamValue(test.input); err != test.want {
 				t.Errorf("failed to validate: str is %v, result is %v, want is %v", str, err, test.want)
 			}
 		})
